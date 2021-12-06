80d8ead5fccd4d537d714f8365708e8918d9de0e
Rituparna Khan <rk18g@h22-login-24.rcc.fsu.edu>
2021-12-06 13:03:32 -0500

    Implemented forward mutation of Greedy algorithm.

--- longitudinalTree.py
+++ longitudinalTree.py
@@ -56,25 +56,28 @@ def validate_subclones(temp_sc, dict_For_tree):
     temp_dict = dict_For_tree.copy()
     for key in temp_sc: # Remember keys here are time and clusters. E.g. t1_c1
         key_value = set(temp_sc[key]) # key_value indicates the SNVs present.
-        #print(key_value)
+        #print("SNVs for a key ",key_value)
         for input_key in dict_For_tree: # Comparing subclones with the original dictionary to validate the subset and superset of clones.
             if key in input_key:
                 #print("Here is the key ..",key)
                 value = set(dict_For_tree[input_key])
-                #print(value)
+                #print(" SNVs of original tree ",value)
                 for sets in key_value:
                     if (set(sets) != value and set(sets).issuperset(value)): # Check if the obtained subclone is a superset of the subclone in previous step.
                         count_usc = count_usc + 1
-                        #print(key," ",sets," --> ",value)
+                        #print("Checking if subclone is superset of time t ",key," ",sets," --> ",value)
                         temp_dict[key+"_"+"usc"+str(count_usc)] = list(sets)
-                    else:
-                        for temp_key in temp_dict: # This else condition helps in checking for all subclones in a given time to make sure the unobserved subclones remains proper superset and subset.
+                        #print(temp_dict)
+                        #print("-------------------")
+                    #else:
+                    #    for temp_key in temp_dict: # This else condition helps in checking for all subclones in a given time to make sure the unobserved subclones remains proper superset and subset.
                             #print(temp_key)
-                            if key in temp_key and "usc" in temp_key:
-                                #print(value," -- ",temp_dict[temp_key])
-                                temp_temp_dict = dict(temp_dict)
-                                del temp_temp_dict[temp_key]
-                                temp_dict = temp_temp_dict.copy() # Update the dict with the proper superclones and subclones. Remove any subclone failing this condition.
+                    #        if key in temp_key and "usc" in temp_key:
+                    #            print(" Rechecking the subset superset ",value," -- ",temp_dict[temp_key])
+                    #            temp_temp_dict = dict(temp_dict)
+                    #            del temp_temp_dict[temp_key]
+                    #            temp_dict = temp_temp_dict.copy() # Update the dict with the proper superclones and subclones. Remove any subclone failing this condition.
+                    #            print("**********",temp_dict)
     return temp_dict
 
 ''' Find the unobserved subclones. '''
@@ -83,14 +86,14 @@ def find_unobserved_subclones(dict_For_tree):
     for key1 in dict_For_tree: # iterating through input_tree. Remember keys here are combination of time and cluster.
         time_point_k = key1.split('_')[1] # we just want to get the times and not the whole key (ex. t_1_c1)
         set_1 =	set(dict_For_tree[key1]) # set_1 indicates the SNVs in a given time and cluster 
-        #print(key_1," length of subset ",len(subset))
+        #print("key 1 ",key1," length of subset ",len(set_1))
         #print(" =========================== ")
         for key2 in dict_For_tree: # comparing subclones of two times pairwise to find subsets.
             if key1 == key2:
                 continue
             time_point_k1 = key2.split('_')[1]
             if int(time_point_k)+1 == int(time_point_k1): # condition to check only between t_1 and t_2 and not between t_1 and t_3. Maintaining the time stepwise.
-                #print(len(subset))
+                #print("Key 2 ",key2)
                 set_2 = set(dict_For_tree[key2]) # set_2 indicates the SNVs in t+1 and each cluster in t+1 
                 if(set_1 != set_2 and set_1.issubset(set_2)): #checking proper subsets
                     usc = find_subsets(set_1,set_2, len(set_2)-1) # find all the subsets for a matching subclone
@@ -178,8 +181,201 @@ def calculate_edge_weight(unobserved_sc_tree, tree_with_children):
             elif(child_snvCount > 0):
                 edge_weight_dict[keytuple] = child_snvCount - parent_snvCount
     return edge_weight_dict
-            
 
+''' Selecting the node s with maximum number of connections.'''
+def select_nodeS(node_list,tree_with_children,nodes_to_remove):
+    maxValue = 0
+    s_node = ""
+    if len(node_list) != 0:
+        for node in node_list:
+            if nodes_to_remove != [] and node in nodes_to_remove:
+                continue
+            node_t1_conn = len(tree_with_children[node])
+            if maxValue == 0 or maxValue < node_t1_conn:
+                maxValue = node_t1_conn
+                s_node = node
+    return s_node
+
+''' Listing the nodes to remove from the tree. '''
+def del_nodes(s_node, node_list):
+    nodes_to_remove = []
+    for node in node_list:
+        if s_node != node:
+            nodes_to_remove.append(node)
+    return nodes_to_remove
+
+''' Selecting A_bar  '''
+def select_A_bar(tree_with_children, t1, s_node):
+    A_bar_nodes = []
+    s_node_children = tree_with_children[s_node]
+    for node in tree_with_children:
+        if t1 in node:
+            if node not in s_node_children and 'usc' not in node:
+                A_bar_nodes.append(node)
+        else:
+            continue
+    return A_bar_nodes
+
+''' Finding any unobserved subclones between time t and union of s_node and A_bar or only s_node. '''
+def enroll_unobservedSc(s_node, A_bar, tree_with_children, unobserved_sc_tree):
+    new_t1_nodes = [] # union of s_node and A_bar. Or just s_node when A_bar is empty.
+    new_t1_nodes_dict = {}
+    t_nodes_dict = {}
+    nodes_to_add = {}
+
+    print(" S node is ",s_node," A bar nodes are ",A_bar)
+    if len(A_bar) != 0:
+        new_t1_nodes.append(s_node)
+        new_t1_nodes.extend(A_bar)
+    else:
+         new_t1_nodes.append(s_node)
+         
+    for node in new_t1_nodes:
+        new_t1_nodes_dict[node] = unobserved_sc_tree[node]
+
+    t_k = s_node.split('_')[1]
+    for node in unobserved_sc_tree: #Taking only the nodes in time t
+        if "t_"+t_k in node and 'usc' not in node:
+            t_nodes_dict[node] = unobserved_sc_tree[node]
+
+    usc_count = 0
+    for t_node in t_nodes_dict:
+        t_node_snv = set(t_nodes_dict[t_node])
+        for t1_node in new_t1_nodes_dict:
+            #print("S node and A_bar nodes .. ",t1_node)
+            t1_node_snv = set(new_t1_nodes_dict[t1_node])
+            if t_node_snv != t1_node_snv and t_node_snv.issubset(t1_node_snv):
+                usc = find_subsets(t_node_snv,t1_node_snv, len(t1_node_snv)-1)
+                for sc in usc:
+                    usc_count = usc_count+1
+                    key = s_node+"_usc"+str(usc_count)
+                    #print("Nodes to add key ",key) #checking below that the node is only proper subset and proper superset
+                    if t_node_snv != set(sc) and t_node_snv.issubset(set(sc)) and t1_node_snv != set(sc) and t1_node_snv.issuperset(set(sc)):
+                        nodes_to_add[key] = sc
+    
+    #print("New t1 nodes ",new_t1_nodes)
+    #print(" t1 dict ",new_t1_nodes_dict)
+    #print(" t dict ",t_nodes_dict)
+    #print(nodes_to_add)
+    return nodes_to_add
+        
+''' Greedy algorithm to remove/add unobserved clones and preserve mutations in the forward direction. '''
+def forward_mutation_one(tree_with_children,unobserved_sc_tree):
+    nodes_to_remove = []
+    nodes_to_add_dict = {}
+    for node_t in tree_with_children:
+        t_children = tree_with_children[node_t]
+        #print(t_children)
+        usc_list = []
+        for subclones in t_children:
+            if 'usc' in subclones:
+                usc_list.append(subclones)
+        #print(node_t+" contains "+str(usc_list))
+        s_node = select_nodeS(usc_list, tree_with_children, nodes_to_remove)
+        #print(s_node) # Check if this is empty before further operations.
+        if del_nodes(s_node, usc_list) != []:
+            nodes_to_remove.extend(del_nodes(s_node, usc_list))
+        print("Nodes to remove ",set(nodes_to_remove)) # Check always if a node is in this list before further operations.
+        
+        t_k = node_t.split('_')[1]
+        for node_t1 in tree_with_children:
+            if node_t == node_t1:
+                continue
+            t1_k1 = node_t1.split('_')[1]
+            if int(t_k)+1 == int(t1_k1):
+                if (s_node != ""):
+                    A_bar_nodes = select_A_bar(tree_with_children, "t_"+t1_k1, s_node)
+                    print("A bar nodes ",A_bar_nodes)
+                    nodes_to_add = enroll_unobservedSc(s_node, A_bar_nodes, tree_with_children, unobserved_sc_tree)
+                    nodes_to_add_dict.update(nodes_to_add)
+                    #print("Nodes to add ",nodes_to_add)
+            t1_children = tree_with_children[node_t1]
+    return nodes_to_remove, nodes_to_add_dict
+
+''' This part searches for unconnected nodes in t+1 (u) and connect them to a node in t (v).'''
+def forward_mutation_two(tree_with_children, unobserved_sc_tree):
+    check_if_node_exists_list = []
+    children_list = []
+    for node_t in unobserved_sc_tree:
+        t_children = tree_with_children[node_t]
+        if t_children == []:
+            continue
+        #print(" Node t child ",t_children)
+        t_k = node_t.split('_')[1]
+        for node_t1 in unobserved_sc_tree:
+            if node_t == node_t1:
+                continue
+            t1_k1 = node_t1.split('_')[1]
+            if int(t_k)+1 == int(t1_k1):
+                #print(" Node t1 ",node_t1)
+                if len(check_if_node_exists_list) != 0:
+                    for node in check_if_node_exists_list:
+                        if node in t_children:
+                            check_if_node_exists_list.remove(node)
+                if node_t1 not in t_children and node_t1 not in children_list:
+                    check_if_node_exists_list.append(node_t1)
+                if node_t1 in t_children:
+                    children_list.append(node_t1)
+    #print(" If child exists node ",set(check_if_node_exists_list))
+
+    if check_if_node_exists_list == []: # Nothing else to do if all nodes are already connected.
+        return None
+
+    node_u_v = {}
+    for lone_node in check_if_node_exists_list: # Adding node u and node v in this loop prior to checking subsets.
+        t1_k1 = lone_node.split('_')[1]
+        max_mutations = 0
+        for t_node in unobserved_sc_tree:
+            if t_node == lone_node:
+                continue
+            t_node_children = tree_with_children[t_node]
+            if 'usc' in t_node or t_node_children == [] or lone_node in t_node_children:
+                continue
+            t_k = t_node.split('_')[1]
+            if int(t_k)+1 == int(t1_k1):
+                snv_count = len(unobserved_sc_tree[t_node])
+                if snv_count > max_mutations:
+                    max_mutations = snv_count
+                    node_u_v[lone_node] = t_node
+
+    print(" Node_u_v ",node_u_v)
+    u_v_conn = {}
+    for node_u in node_u_v: # Checking if mutations in node v is a proper subset of mutations in node u.
+        node_v = node_u_v[node_u]
+        node_u_snv_set = set(unobserved_sc_tree[node_u])
+        node_v_snv_set = set(unobserved_sc_tree[node_v])
+        if(node_u_snv_set != node_v_snv_set and node_v_snv_set.issubset(node_u_snv_set)):
+            u_v_conn[node_u] = node_v
+
+    print(" Node_v_u ",u_v_conn)
+    return u_v_conn
+
+''' Method to update the longitudinal tree with the nodes. ''' 
+def update_longitudinal_tree(unobserved_sc_tree, nodes_to_remove, nodes_to_add_dict, u_v_conn):
+    # Update unobserved_sc_tree by removing nodes_to_remove and adding values from nodes_to_add_dict.
+    # Then call construct_tree function to get the tree with updated nodes.
+    # Then add u_v_conn nodes to the constructed tree.
+    temp_sc_tree = unobserved_sc_tree.copy()
+    print("Nodes to del ",nodes_to_remove)
+    print(" Nodes to add ",nodes_to_add_dict)
+    for node in nodes_to_remove:
+        if node in temp_sc_tree:
+            print("Node to del ",node)
+            del temp_sc_tree[node]
+
+    for key in nodes_to_add_dict:
+        temp_sc_tree[key] = nodes_to_add_dict[key]
+
+    print(" After adding and deleting node ",temp_sc_tree)
+    updated_tree = construct_tree(temp_sc_tree)
+    if u_v_conn == {} or u_v_conn == None:
+        return updated_tree
+    else:
+        updated_tree_copy = updated_tree.copy()
+        for u_v in u_v_conn:
+            updated_tree_copy[u_v] = u_v_conn[u_v]
+        return updated_tree
+    
 parser = argparse.ArgumentParser()
 parser.add_argument("-input", "--input",dest ="input", help="Input matrix (Input to the Longitudinal tree)")
 args = parser.parse_args()
@@ -189,11 +385,15 @@ inputToTree = convertToPandas(tsv_input)
 clones_in_time = convert_InputToDict(inputToTree)
 print("Step 1 to get the clones arranged ... ",clones_in_time)
 unobserved_sc_tree = find_unobserved_subclones(clones_in_time)
+
 tree_with_children = construct_tree(unobserved_sc_tree)
 print(" Tree with children ",tree_with_children)
-edge_weight_dict = calculate_edge_weight(unobserved_sc_tree, tree_with_children)
-print(" Tree with edge weights ",edge_weight_dict)
-
+#edge_weight_dict = calculate_edge_weight(unobserved_sc_tree, tree_with_children)
+#print(" Tree with edge weights ",edge_weight_dict)
+nodes_to_remove, nodes_to_add_dict = forward_mutation_one(tree_with_children, unobserved_sc_tree)
+u_v_conn = forward_mutation_two(tree_with_children, unobserved_sc_tree)
+updated_tree = update_longitudinal_tree(unobserved_sc_tree, nodes_to_remove, nodes_to_add_dict, u_v_conn)
+print("Updated Longitudinal tree ",updated_tree)
 #left = Tree("left","left_data")
 #middle = Tree("middle","middle_data")
 #right = Tree("right","right_data")
